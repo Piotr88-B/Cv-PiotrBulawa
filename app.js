5a75cbf8d024698407841274c1b1913b2ee42e49
Piotr Buława <piotrbulawa88@gmail.com>
2022-02-03 04:54:11 +0100

    add menu hamburger

--- app.js
+++ app.js
@@ -14,17 +14,40 @@ const selectArt = document.querySelector('.article-2')
 
 
 const showDiv = () => {
-  artInside1.style.display = "block"; 
+  artInside1.style.display = "block";
 }
 
-const functionAS= ()=>{
-  selectArt.style.transform ='2s';
+const functionAS = () => {
+  selectArt.style.transform = '2s';
 }
 
 const hiddenDiv = () => {
-  artInside1.style.display = "none"; 
+  artInside1.style.display = "none";
 }
-
 
 selectArt.addEventListener('mouseenter', showDiv)
 selectArt.addEventListener('mouseleave', hiddenDiv)
+
+// menu hamburger
+
+const selectUlMenu = document.querySelector('.ul-menu')
+const selectOpenMenu = document.querySelector('.openMenu')
+const selectclosMenu = document.querySelector('.closMenu')
+const selectLink = selectUlMenu.querySelectorAll('.linkNav')
+
+const showFunction = () => {
+  selectUlMenu.style.display = 'flex';
+  selectUlMenu.style.top = '0'
+}
+
+const closeFunction = () => {
+  selectUlMenu.style.top = '-100%';
+}
+
+selectLink.forEach(e => e.addEventListener('click', ()=>{
+     selectUlMenu.style.top = '-100%';
+}))
+
+
+selectOpenMenu.addEventListener('click', showFunction);
+selectclosMenu.addEventListener('click', closeFunction);
